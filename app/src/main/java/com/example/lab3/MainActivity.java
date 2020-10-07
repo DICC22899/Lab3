f808f66f09660fa62a0c41604c9270374f5aa1c7
DICC22899 <66987321+DICC22899@users.noreply.github.com>
2020-10-07 08:56:44 -0500

    COMMIT # : LAB 3 salyudo

--- app/src/main/java/com/example/lab3/MainActivity.java
+++ app/src/main/java/com/example/lab3/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.lab3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button saludo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        saludo=(Button)findViewById(R.id.saludo);
+        saludo.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SaludoActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
